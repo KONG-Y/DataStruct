83f29492551a2c60d463845a36332a3f563a7bba
KONG-Y <31161653+KONG-Y@users.noreply.github.com>
2020-06-04 22:04:24 +0800

    Update 顺序表.cpp

--- 表/顺序表.cpp
+++ 表/顺序表.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class list {
 public:
 	list();
-	int Length()const;								//表的长度
-	int Get_element(const int index)const;			//按序号查找元素，返回值
-	int Locate(const int value)const;				//按之查找元素，返回序号
+	int Length()const;				//表的长度
+	int Get_element(const int index)const;		//按序号查找元素，返回值
+	int Locate(const int value)const;		//按之查找元素，返回序号
 	void Insert(const int index, const int value);	//插入（在index位置前面插入）
-	void Delete(const int index);					//删除
+	void Delete(const int index);			//删除
 private:
 	int data[maxlen];//数据域
-	int count;		 //表长度
+	int count;   	 //表长度
 };
 
 list::list() {
@@ -33,21 +33,21 @@ int list::Get_element(const int index)const {
 }
 
 void list::Insert(const int index, const int value) {
-	if (count != maxlen) {									 //未溢出
-		if (index >= 1 && index < Length() + 1) {			 //符合后移范围
+	if (count != maxlen) {						 //未溢出
+		if (index >= 1 && index < Length() + 1) {		 //符合后移范围
 			for (int i = Length() - 1; i >= index - 1; i--) {//位置后移（插在index位置前面，所以 i >= index - 1）
 				data[i + 1] = data[i];
 			}
 		}
 		data[index] = value;//插入数值
-		count++;			//表长度加1
+		count++;	    //表长度加1
 		
 	}
 }
 
 void list::Delete(const int index) {
-	if (Length() != 0) {							            //未空
-		if (index < Length()) {						  //符合删除范围
+	if (Length() != 0) {					  //未空
+		if (index < Length()) {				  //符合删除范围
 			for (int i = index; i < Length()-1; i++) {//位置前移
 				data[i] = data[i + 1];
 			}
