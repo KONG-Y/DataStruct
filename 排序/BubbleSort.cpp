8d7dac2ae4272e976663a3a20dbbab512d0c6242
KONG-Y <31161653+KONG-Y@users.noreply.github.com>
2023-11-06 23:22:29 +0800

    Create BubbleSort.cpp

---
+++ 排序/BubbleSort.cpp
@@ -0,0 +1,14 @@
+/*-------------------------------------冒泡排序---------------------------------------*/
+//时间复杂度：最好O(n),最坏O（n^2）
+//稳定
+
+
+void BubbleSort(vector<int>& nums) {
+	for (int i = 0; i < nums.size() - 1; i++) {
+		for (int j = 0; j < nums.size() - 1 - i; j++) {
+			if (nums[j] > nums[j + 1]) {
+				swap(nums[j], nums[j + 1]);
+			}
+		}
+	}
+}
